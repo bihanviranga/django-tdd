3ae6183ecca63e2bc2ff274772811f9fe5245be2
Bihan Viranga <bihanviranga@gmail.com>
2020-04-02 12:45:06 +0530

    FT to accomodate unique URLs

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -33,7 +33,7 @@ class NewVisitorTest(LiveServerTestCase):
 					raise e
 				time.sleep(0.5)
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
+	def test_can_start_a_list_for_one_user(self):
 		# Bihan has heard about a cool new online to-do app.
 		# He goes to check out its homepage.
 		self.browser.get(self.live_server_url)
@@ -68,14 +68,54 @@ class NewVisitorTest(LiveServerTestCase):
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-		# Bihan wonders whether the site will remember his list.
-		# Then he sees that the site has generated a unique URL for him
-		# there is some explanatory text to that effect.
-		self.fail('Finish the test!')
-
 		# He visits that URL, and his todo-list is still there.
 
 		# Satisfied, he goes back to sleep.
+
+	def test_multiple_users_can_start_lists_at_different_urls(self):
+		# Bihan starts a new to-do list
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+		# He notices that his list has a unique URL
+		bihan_list_url = self.browser.current_url
+		self.assertRegex(bihan_list_url, '/lists/.+')
+
+		# Now a new user, Viranga, comes alnog to the site.
+
+		## We use a new browser session to make sure that no information
+		## of Bihan's is coming through from cookies, etc
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		# Viranga visits the home page. There is no sign of
+		# Bihan's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertNotIn('make a fly', page_text)
+
+		# Viranga starts a new list by entering a new item.
+		# He is less interesting than Bihan..
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_list_table('1: Buy milk')
+
+		# Viranga gets his own unique URL
+		viranga_list_url = self.browser.current_url
+		self.assertRegex(viranga_list_url, '/lists/.+')
+		self.assertNotEqual(viranga_list_url, bihan_list_url)
+
+		# Again, there is no trace of Bihan's list
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		self.assertIn('Buy milk', page_text)
+
+		# Satisfied, they both go back to sleep
 
 # Since we are moving to Django LiveServerTestCase from unittest,
 # name-main block is not needed (right?)
